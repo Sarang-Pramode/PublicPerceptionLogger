b2d65e54eb4b6169a973c27820182812fd0e8725
Sarang-Pramode <sarangpramode1997@gmail.com>
2023-03-21 14:44:16 -0400

    added postgres db

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import psycopg2
 
 app = Flask(__name__)
 
-visits = 0
-
-@app.before_request
-def count_visits():
-    global visits
-    if request.host == 'www.soteriia.com':
-        visits += 1
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route('/')
 def index():
-    return render_template('index.html', visits=visits)
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM clicks;')
+    count = cur.fetchone()[0]
+    cur.close()
+    return render_template('index.html', visits=count)
+
+@app.route('/button-clicked', methods=['POST'])
+def button_clicked():
+    cur = conn.cursor()
+    cur.execute('INSERT INTO clicks (timestamp) VALUES (NOW());')
+    conn.commit()
+    cur.close()
+    return redirect(url_for('redirect_to_soteriia'))
+
+@app.route('/redirect-to-soteriia')
+def redirect_to_soteriia():
+    return redirect('http://www.soteriia.com')
 
 if __name__ == '__main__':
     app.run(debug=True)
