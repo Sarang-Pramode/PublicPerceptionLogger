8d68d1d5a7ce3494852c1620021ce461e5472175
Sarang-Pramode <sarangpramode1997@gmail.com>
2023-03-21 13:59:05 -0400

    visit counter added

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+visits = 0
+
+@app.before_request
+def count_visits():
+    global visits
+    if request.host == 'www.soteriia.com':
+        visits += 1
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', visits=visits)
 
 if __name__ == '__main__':
     app.run(debug=True)
